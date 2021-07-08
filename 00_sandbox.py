3c3c0e884081f1611d66ab18bd6574295bb69cd5
EllaHunt <hunt.ella@awatapu.school.nz>
2021-07-09 10:24:29 +1200

    time 5

---
+++ 00_sandbox.py
@@ -0,0 +1,9 @@
+# Ask users for a number
+get_number = int(input("choose a number? "))
+
+# Multiply the number by 5
+times_5 = get_number * 5
+
+# output the result
+print("{} times 5 is equal to "
+      "{}".format(get_number, times_5))
